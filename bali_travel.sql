c0bc1172748dca0e998285b1aea74784d359f4f9
MohammadFarisF <farisfawwaz18@gmail.com>
2024-12-15 13:15:25 +0700

    Update bali_travel.sql

--- bali_travel.sql
+++ bali_travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 03, 2024 at 03:40 AM
+-- Generation Time: Dec 15, 2024 at 06:14 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -148,8 +148,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`user_id`, `full_name`, `email`, `password`, `phone_number`, `user_role`, `photo`, `created_at`, `updated_at`) VALUES
-('U001', 'Mohammad Faris Fawwaz', 'farisfawwaz123@gmail.com', '$2y$10$PgHv21Iba7PotRA.NeFla.HX1tNelgVlIAqmlPvdzK4WHd8QSLkia', '081234567891', 'owner', '1730129683_bf5804dbb1ce67bf8530.jpg', '2024-10-07 02:03:00', '2024-11-03 21:35:51'),
-('U002', 'Muhammad Fauzan Azhar', 'mfauzanazhar12@gmail.com', '$2y$10$CCrIoiQ77QgPRrON3e8//OaMHrdauZZ3HWxjMDd6w4R1PQQsUBy8a', '089843213412', 'admin', '1730128767_15f3f8e75d79db270b7d.png', '2024-10-07 02:07:29', '2024-10-28 15:19:27');
+('U003', 'Teguh Pratama', 'teguhpratama@gmail.com', '$2y$10$UjjRopX9tEwbxP1S6bYObeSqs6H1ZFLodZ5WM0LnsIK0YKLI.WzJO', '081456286315', 'owner', '1734225146_faee40e7fec61cf51392.jpg', '2024-12-15 01:12:26', NULL),
+('U004', 'Jaka Jayanto', 'jakajayanto@mail.com', '$2y$10$odXt2omStdwzFzB8QexTAOt47lm8QpR4kw904XB.iplzj7Hzxtk4a', '081234567891', 'admin', '1734225235_07fdb60e50d9da880365.jpeg', '2024-12-15 01:13:56', NULL);
 
 -- --------------------------------------------------------
 
@@ -172,10 +172,10 @@ CREATE TABLE `bank_travel` (
 --
 
 INSERT INTO `bank_travel` (`trabank_id`, `account_number`, `account_holder_name`, `bank_name`, `photo`, `created_at`, `updated_at`) VALUES
-(1, '1412341323', 'Muhammad Fauzan Azhar', 'Bank Central Asia (BCA)', 0x313732383239333130375f61333861613334663230383463323532653630322e6a7067, '2024-10-07 02:25:07', '2024-10-07 02:31:44'),
-(2, '1234567891', 'Mohammad Faris Fawwaz', 'Bank Negara Indonesia (BNI)', 0x313732383239333331375f38306361666532663237303039613530313731632e706e67, '2024-10-07 02:28:37', '2024-10-07 02:31:33'),
-(3, '081234567891', 'Mohammad Faris Fawwaz', 'Paypal', 0x313732383239333431385f30623536316462306534623862623136623563382e6a7067, '2024-10-07 02:30:18', '2024-10-07 02:31:23'),
-(4, '081256789423', 'Muhammad Fauzan Azhar', 'DANA', 0x313732383239333436395f61626134653835613532353164643735343537382e6a706567, '2024-10-07 02:31:09', '2024-10-07 02:31:09');
+(1, '1412341323', 'Muhammad Fadil Ilham', 'Bank Central Asia (BCA)', 0x313732383239333130375f61333861613334663230383463323532653630322e6a7067, '2024-10-07 02:25:07', '2024-12-15 04:10:19'),
+(2, '1234567891', 'Wahyu Sagita', 'Bank Negara Indonesia (BNI)', 0x313732383239333331375f38306361666532663237303039613530313731632e706e67, '2024-10-07 02:28:37', '2024-12-15 04:10:52'),
+(3, '081234567891', 'Fajar Anwar', 'Paypal', 0x313732383239333431385f30623536316462306534623862623136623563382e6a7067, '2024-10-07 02:30:18', '2024-12-15 04:11:08'),
+(4, '081256789423', 'Giorgino Permata', 'DANA', 0x313732383239333436395f61626134653835613532353164643735343537382e6a706567, '2024-10-07 02:31:09', '2024-12-15 04:11:45');
 
 -- --------------------------------------------------------
 
@@ -204,8 +204,12 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`booking_id`, `customer_id`, `package_id`, `address`, `total_people`, `departure_date`, `return_date`, `total_amount`, `cust_request`, `booking_status`, `payment_status`, `created_at`, `updated_at`) VALUES
+('B17670', 'C002', 'P03', 'Jl. Medan', 2, '2024-12-31', '2025-01-02', '2000000.00', '', 'pending', 'pending', '2024-12-15 05:19:16', NULL),
 ('B32535', 'C002', 'P02', 'Jl. Jakarta', 1, '2024-11-20', '2024-11-20', '750000.00', 'Tidak Ada', 'completed', 'paid', '2024-11-16 19:08:56', '2024-11-25 11:09:02'),
+('B37524', 'C002', 'P02', 'Jl. Jakarta', 6, '2024-12-16', '2024-12-16', '4500000.00', '', 'pending', 'pending', '2024-12-15 04:56:03', NULL),
 ('B43298', 'C002', 'P01', 'Jl. Medan', 4, '2024-11-17', '2024-11-17', '3000000.00', '2 Anak, 2 Dewasa', 'completed', 'paid', '2024-11-16 19:06:26', '2024-11-17 04:23:43'),
+('B49198', 'C002', 'P02', 'Jl. Bandung', 2, '2024-12-29', '2024-12-29', '1500000.00', '', 'pending', 'pending', '2024-12-15 05:19:43', NULL),
+('B49716', 'C002', 'P01', 'Jl. Medan', 5, '2024-12-30', '2024-12-30', '3750000.00', '1 Anak - Anak', 'confirmed', 'paid', '2024-12-10 01:45:34', '2024-12-10 01:46:41'),
 ('B68127', 'C002', 'P01', 'Jl. Medan', 2, '2024-11-25', '2024-11-25', '1500000.00', '', 'completed', 'paid', '2024-11-24 18:26:03', '2024-11-25 11:28:47');
 
 --
@@ -257,7 +261,13 @@ INSERT INTO `booking_destinations` (`booking_destination_id`, `booking_id`, `des
 (18, 'B43298', 'D02'),
 (19, 'B32535', 'D03'),
 (21, 'B68127', 'D01'),
-(22, 'B68127', 'D02');
+(22, 'B68127', 'D02'),
+(28, 'B49716', 'D01'),
+(29, 'B49716', 'D02'),
+(60, 'B37524', 'D03'),
+(61, 'B17670', 'D04'),
+(62, 'B17670', 'D05'),
+(63, 'B49198', 'D03');
 
 -- --------------------------------------------------------
 
@@ -278,7 +288,8 @@ CREATE TABLE `booking_vehicles` (
 INSERT INTO `booking_vehicles` (`booking_vehicle_id`, `booking_id`, `vehicle_id`) VALUES
 (8, 'B43298', 10),
 (9, 'B32535', 7),
-(10, 'B68127', 10);
+(10, 'B68127', 10),
+(12, 'B49716', 7);
 
 -- --------------------------------------------------------
 
@@ -308,8 +319,7 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`customer_id`, `nik`, `email`, `password`, `full_name`, `phone_number`, `photo`, `citizen`, `tgl_lahir`, `gender`, `user_role`, `created_at`, `updated_at`) VALUES
 ('C001', NULL, 'mfirjatullah123@gmail.com', '$2y$10$buajdfrcMRWhvt.RTZFUN.vS6NKh5dPDQVngJ5xU1KrJFlcJCvyWa', 'Muhammad Firjatullah', '081234513423', NULL, NULL, NULL, NULL, 'customer', '2024-11-03 14:16:26', '2024-11-08 02:47:08'),
-('C002', '123456789101112', 'agushariyanto@gmail.com', '$2y$10$OLYi3vMUgQWO4Re8Me0w9O6FsuEEuerMeETBEQO1NCw41wz0KHkV2', 'Agus Hariyanto', '+6281235826974', '1731036860_6b4cd01a49c7091b5cb6.jpg', 'Indonesia', '15 June 2004', 'laki-laki', 'customer', '2024-11-07 20:18:37', '2024-11-08 07:06:55'),
-('C003', NULL, 'teguhpratama@gmail.com', '$2y$10$9G4eAaSrHNd.khbYNTxYieDr1oyVBCzeNXeEr8uMDX3YJW7WOfbX.', 'Teguh Pratama', '+6281456286315', NULL, NULL, NULL, NULL, 'customer', '2024-11-09 18:16:34', '2024-11-10 01:16:34');
+('C002', '123456789101112', 'agushariyanto@gmail.com', '$2y$10$OLYi3vMUgQWO4Re8Me0w9O6FsuEEuerMeETBEQO1NCw41wz0KHkV2', 'Agus Hariyanto', '+6281235826974', '1731036860_6b4cd01a49c7091b5cb6.jpg', 'Indonesia', '15 June 2004', 'laki-laki', 'customer', '2024-11-07 20:18:37', '2024-11-08 07:06:55');
 
 --
 -- Triggers `customer`
@@ -373,7 +383,12 @@ CREATE TABLE `destinations` (
 INSERT INTO `destinations` (`destination_id`, `package_id`, `destination_name`, `longitude`, `latitude`, `harga_per_orang`, `description`, `foto`, `created_at`, `updated_at`) VALUES
 ('D01', 'P01', 'Rice Terrace Swing', '115.27914568465303', '-8.434760395434676', '500000.00', 'the most popular choice in Bali recently. The tourist destination is located on the north side of Ubud Village, Gianyar Regency.', '1730131800_146ff9732e20b166befb.png', '2024-10-28 09:10:00', '2024-10-28 09:10:00'),
 ('D02', 'P01', 'Goa Gajah Temple', '115.2871319949841', '-8.52340219151913', '250000.00', 'famously with a historical temple, an archaelogical place. An Archaelogical and Historical Temple in Bali.', '1730131938_56b2a71e91c0be230bd7.jpg,1730131938_9d37b01be6047f3a35b0.jpg', '2024-10-28 09:12:18', '2024-10-28 09:12:18'),
-('D03', 'P02', 'Tanah Lot', '115.0868031452482', '-8.620999689160582', '750000.00', 'Bedugul is a famous mountain tourist area in Bali, Indonesia. Located in Tabanan Regency, about 50 kilometers from Denpasar, Bedugul is a favorite destination because of its cool air and beautiful natural views. Bedugul is surrounded by green hills, lakes and lush gardens.', '1730132001_25f9ee03d8ce6855a035.jpg', '2024-10-28 09:13:21', '2024-10-28 09:13:21');
+('D03', 'P02', 'Tanah Lot', '115.0868031452482', '-8.620999689160582', '750000.00', 'Bedugul is a famous mountain tourist area in Bali, Indonesia. Located in Tabanan Regency, about 50 kilometers from Denpasar, Bedugul is a favorite destination because of its cool air and beautiful natural views. Bedugul is surrounded by green hills, lakes and lush gardens.', '1730132001_25f9ee03d8ce6855a035.jpg', '2024-10-28 09:13:21', '2024-10-28 09:13:21'),
+('D04', 'P03', 'Pulau Komodo', '119.47536248848921', '-8.528766544484318', '500000.00', 'Pulau Komodo adalah salah satu destinasi utama di Labuan Bajo dan bagian dari Taman Nasional Komodo. Pulau ini terkenal sebagai habitat asli Komodo, kadal purba raksasa yang hanya ditemukan di Indonesia. Pengunjung bisa melakukan trekking untuk melihat komodo di habitat aslinya, sambil menikmati pemandangan bukit berbatu dan pantai yang eksotis.', '1733799262_af74385a9b5b18ff5e4a.jpg,1733799262_bf38f72d3e1b88d26236.jpg', '2024-12-10 03:21:25', '2024-12-10 03:21:25'),
+('D05', 'P03', 'Pulau Padar', '119.58085601327504', '-8.648227190703077', '500000.00', 'Pulau Padar adalah pulau ketiga terbesar di Taman Nasional Komodo. Pulau ini terkenal dengan pemandangan spektakulernya dari puncak bukit. Untuk mencapai puncak, pengunjung harus melakukan pendakian singkat, dan dari sana, mereka akan disuguhkan panorama menakjubkan berupa perbukitan, pantai berpasir putih, hitam, dan merah yang membentuk teluk-teluk indah.', '1733799440_2c2ba22b8a89633b2210.jpg,1733799440_e202604e7c3fc09651ce.jpg', '2024-12-10 03:15:54', '2024-12-10 03:15:54'),
+('D06', 'P03', 'Pantai Merah Muda', '119.52034930991636', '-8.60119720256333', '500000.00', 'Pink Beach atau Pantai Merah merupakan salah satu pantai unik di dunia dengan pasir berwarna merah muda. Warna pink ini berasal dari campuran pasir putih dan pecahan foraminifera (organisme laut mikro). Pantai ini juga menjadi tempat yang ideal untuk snorkeling dan diving karena terumbu karang dan kehidupan bawah lautnya yang mempesona.', '1733799650_4044c218932bddfcc408.jpg,1733799650_ac7eea31d967d29a65df.jpg', '2024-12-10 03:00:50', '2024-12-10 03:00:50'),
+('D07', 'P03', 'Gua Rangko (Gua Air Asin)', '119.963973166282', '-8.432580775980849', '500000.00', 'Gua Rangko adalah gua yang memiliki kolam air asin berwarna biru jernih di dalamnya. Cahaya matahari yang masuk melalui celah gua membuat airnya berkilau seperti kristal. Gua ini sangat cocok bagi wisatawan yang ingin berenang atau sekadar menikmati suasana alami yang menenangkan.', '1733799817_bd2b74ed3dc5c37b22fa.jpg,1733799817_602f64895c1fb8ad2124.jpg', '2024-12-10 03:03:37', '2024-12-10 03:03:37'),
+('D08', 'P03', 'Air Terjun Cunca Wulang', '119.99392866628388', '-8.54504233306012', '500000.00', 'Cunca Wulang adalah air terjun yang terletak di tengah hutan tropis. Untuk mencapainya, pengunjung harus melakukan trekking melewati hutan. Air terjun ini memiliki kolam alami berwarna hijau kebiruan yang cocok untuk berenang dan lompat tebing.', '1733800140_53d10d1837ff87f2f557.jpg,1733800140_0226345ea5c97bfecb7b.jpg', '2024-12-10 03:09:00', '2024-12-10 03:09:00');
 
 -- --------------------------------------------------------
 
@@ -398,7 +413,8 @@ CREATE TABLE `packages` (
 
 INSERT INTO `packages` (`package_id`, `package_name`, `package_type`, `hari`, `description`, `foto`, `created_at`, `updated_at`) VALUES
 ('P01', 'Paket Wisata Ubud', 'single_destination', 1, 'Ubud adalah destinasi wisata yang unik dan memikat, menawarkan perpaduan sempurna antara alam, budaya, dan seni. Meskipun semakin populer, Ubud berhasil mempertahankan pesona tradisional dan suasana tenangnya, yang menjadi ciri khas daerah ini. Dengan keindahan alam yang menakjubkan, warisan budaya yang kaya, dan lingkungan yang damai, Ubud menjadi salah satu permata tersembunyi Bali dan wajib dikunjungi oleh para wisatawan.', '1729568758_e309f0d7e9cae72dcd8a.jpg', NULL, '2024-12-02 23:18:22'),
-('P02', 'Paket Wisata Bedugul', 'single_destination', 1, 'Bedugul adalah kawasan wisata pegunungan yang terkenal di Bali, Indonesia. Terletak di Kabupaten Tabanan, sekitar 50 kilometer dari Denpasar, Bedugul menjadi destinasi favorit karena udara sejuknya dan pemandangan alam yang indah. Dikelilingi oleh perbukitan hijau, danau, dan taman yang subur, Bedugul menawarkan suasana yang menenangkan dan pemandangan alam yang memukau.', '1729568779_5b6aa544ebe5b64901ab.jpg', NULL, '2024-12-02 23:18:48');
+('P02', 'Paket Wisata Bedugul', 'single_destination', 1, 'Bedugul adalah kawasan wisata pegunungan yang terkenal di Bali, Indonesia. Terletak di Kabupaten Tabanan, sekitar 50 kilometer dari Denpasar, Bedugul menjadi destinasi favorit karena udara sejuknya dan pemandangan alam yang indah. Dikelilingi oleh perbukitan hijau, danau, dan taman yang subur, Bedugul menawarkan suasana yang menenangkan dan pemandangan alam yang memukau.', '1729568779_5b6aa544ebe5b64901ab.jpg', NULL, '2024-12-02 23:18:48'),
+('P03', 'Paket Wisata Labuan Bajo', 'multiple_day', 3, 'Labuan Bajo adalah sebuah kota nelayan kecil yang terletak di ujung barat Pulau Flores, Indonesia. Kota ini berfungsi sebagai gerbang menuju Taman Nasional Komodo yang terkenal di dunia, rumah bagi komodo yang terkenal. Selama bertahun-tahun, Labuan Bajo telah bertransformasi dari desa nelayan yang tenang menjadi pusat pariwisata yang berkembang pesat, menarik wisatawan dari seluruh dunia.', '1733795688_8186d221d626e79908cc.jpg', NULL, '2024-12-10 01:56:40');
 
 -- --------------------------------------------------------
 
@@ -426,7 +442,8 @@ CREATE TABLE `payments` (
 INSERT INTO `payments` (`payment_id`, `booking_id`, `customer_id`, `payment_method`, `account_name`, `account_number`, `account_holder_name`, `payment_date`, `payment_status`, `proof_of_payment`) VALUES
 (7, 'B43298', 'C002', 'bank_transfer', 'Bank Rakyat Indonesia', '113414134134', 'Agus Hariyanto', '2024-11-17 02:06:57', 'validated', '1731809217_65fe52845770bec23769.jpg'),
 (9, 'B32535', 'C002', 'bank_transfer', 'Bank Rakyat Indonesia', '113414134134', 'Agus Hariyanto', '2024-11-17 03:15:03', 'validated', '1731813303_bec3a590b7bf2da3740c.jpg'),
-(10, 'B68127', 'C002', 'bank_transfer', 'Bank Rakyat Indonesia', '113414134134', 'Agus Hariyanto', '2024-11-25 01:30:23', 'validated', '1732498223_9363ca8148fd87244278.png');
+(10, 'B68127', 'C002', 'bank_transfer', 'Bank Rakyat Indonesia', '113414134134', 'Agus Hariyanto', '2024-11-25 01:30:23', 'validated', '1732498223_9363ca8148fd87244278.png'),
+(13, 'B49716', 'C002', 'bank_transfer', 'Bank Rakyat Indonesia', '1042225645', 'Faris Fawwaz', '2024-12-10 01:46:16', 'validated', '1733795176_27f6a3ff6e0ae6ddec9b.jpg');
 
 --
 -- Triggers `payments`
@@ -524,9 +541,9 @@ CREATE TABLE `vehicles` (
 --
 
 INSERT INTO `vehicles` (`vehicle_id`, `vehicle_name`, `license_plate`, `capacity`, `vehicle_type`, `vehicle_photo`, `status`, `created_at`, `updated_at`) VALUES
-(7, 'Avanza Black', 'DK 1234 AB', 6, 'MPV', '1727757860_3598cad48cb5adbdbd31.jpg', 'in_use', '2024-09-30 21:44:20', '2024-12-03 03:38:30'),
-(9, 'Suzuki APV Putih', 'DK 1234 ABC', 8, 'APV', '1728292802_0e76ce8c9e2331e8ddef.png', 'in_use', '2024-10-07 02:20:02', '2024-12-03 02:32:02'),
-(10, 'Suzuki Ertiga Abu - Abu', 'DK 3456 BCD', 5, 'MPV', '1728292900_1fe2e62667d6773573d4.png', 'in_use', '2024-10-07 02:21:40', '2024-12-03 02:32:09');
+(7, 'Avanza Black', 'DK 1234 AB', 6, 'MPV', '1727757860_3598cad48cb5adbdbd31.jpg', 'available', '2024-09-30 21:44:20', '2024-12-15 02:19:17'),
+(9, 'Suzuki APV Putih', 'DK 1234 ABC', 8, 'APV', '1728292802_0e76ce8c9e2331e8ddef.png', 'available', '2024-10-07 02:20:02', '2024-12-15 02:19:21'),
+(10, 'Suzuki Ertiga Abu - Abu', 'DK 3456 BCD', 5, 'MPV', '1728292900_1fe2e62667d6773573d4.png', 'available', '2024-10-07 02:21:40', '2024-12-15 02:19:24');
 
 --
 -- Indexes for dumped tables
@@ -631,19 +648,19 @@ ALTER TABLE `bank_travel`
 -- AUTO_INCREMENT for table `booking_destinations`
 --
 ALTER TABLE `booking_destinations`
-  MODIFY `booking_destination_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `booking_destination_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `booking_vehicles`
 --
 ALTER TABLE `booking_vehicles`
-  MODIFY `booking_vehicle_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `booking_vehicle_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `payment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `refunds`
